41b448a697ff7d3f22e91109d8a0cd1dab9780cb
Ilarion Golovin <ilarion.golovin@gmail.com>
2026-09-22 05:20:13 +0300

    1

--- Build/Web.loader.js
+++ Build/Web.loader.js
@@ -50,7 +50,7 @@ function createUnityInstance(canvas, config, onProgress = () => { }) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 53871191,
+    wasmFileSize: 53889041,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     onProgress,
